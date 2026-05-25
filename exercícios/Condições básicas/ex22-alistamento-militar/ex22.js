567ffba29f43e87064024ceb1cff6201209e2b8b
nicolas-gabriel020 <gabrielnicolas018@gmail.com>
2026-05-25 11:22:35 -0300

    Exercício 22 adicionado

--- exercícios/Condições básicas/ex22-alistamento-militar/ex22.js
+++ exercícios/Condições básicas/ex22-alistamento-militar/ex22.js
@@ -2,5 +2,40 @@ let botao = document.querySelector('#botao')
 botao.addEventListener('click', calculo)
 
 function calculo() {
-    
+    let res = document.querySelector('#res')
+    let ano = document.querySelector('#ano')
+
+    res.innerHTML = ''
+    res.style.background = ''
+
+    if (ano.value == '') {
+        alert('Por favor preencha todos os campos!')
+    } else {
+        let ano_formatado = Number(ano.value)
+        let ano_atual = new Date().getFullYear()
+        
+        if (ano_formatado > ano_atual) {
+            alert('Por favor preencha com um ano válido!')
+        } else {
+            let idade = ano_atual - ano_formatado
+
+            if (idade < 18) {
+                res.innerHTML = `Ano atual: ${ano_atual} <br>
+                Idade: ${idade} <br>
+                Faltam ${18 - idade} anos para o alistamento!`
+                res.style.background = 'yellow'
+            } else if (idade > 18) {
+                res.innerHTML = `Ano atual: ${ano_atual} <br>
+                Idade: ${idade} <br>
+                Já se passaram ${idade - 18} anos do seu alistamento militar!`
+                res.style.background = 'red'
+            } else {
+                res.innerHTML = `Ano atual: ${ano_atual} <br>
+                Idade: ${idade} <br>
+                Já está na hora de se alistar!`
+                res.style.background = 'green'
+            }
+        }
+
+    }
 }
