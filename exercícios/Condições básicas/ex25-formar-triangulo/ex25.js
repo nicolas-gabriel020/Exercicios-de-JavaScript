e1b877a21303c7969d19db81ef5f461a3034c76c
nicolas-gabriel020 <gabrielnicolas018@gmail.com>
2026-06-02 15:54:58 -0300

    Exercício 25 adicionado

---
+++ exercícios/Condições básicas/ex25-formar-triangulo/ex25.js
@@ -0,0 +1,44 @@
+const botao = document.querySelector('#botao')
+botao.addEventListener('click', calcular)
+
+function calcular() {
+    let res = document.querySelector('#res')
+
+    res.innerHTML = ''
+    res.style.background = ''
+
+    let valor1 = document.querySelector('#valor1')
+    let valor2 = document.querySelector('#valor2')
+    let valor3 = document.querySelector('#valor3')
+
+    if (valor1.value == '' || valor2.value == '' || valor3.value == '') {
+        alert('Por favor preencha todos os campos!')
+    } else {
+        let ladoA = Number(valor1.value)
+        let ladoB = Number(valor2.value)
+        let ladoC = Number(valor3.value)
+
+        if (ladoA <= 0 || ladoB <= 0 || ladoC <= 0) {
+            alert('Por favor preencha com valores válidos!')
+        } else {
+            if (ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA) {
+                let perimetro = ladoA + ladoB + ladoC
+                
+                res.innerHTML = `Triângulo válido! <br>
+                Lado A: ${ladoA} <br>
+                Lado B: ${ladoB} <br>
+                Lado C: ${ladoC} <br>
+                Perimetro: ${perimetro}`
+
+                res.style.background = 'green'
+            } else {
+                res.innerHTML = `Não é possivel formar um triângulo! <br>
+                Lado A: ${ladoA} <br>
+                Lado B: ${ladoB} <br>
+                Lado C: ${ladoC} <br>`
+
+                res.style.background = 'red'
+            }
+        }
+    }
+}
