28160e0a0543e6623376cbb7ae9f9c9351b8611a
nicolas-gabriel020 <gabrielnicolas018@gmail.com>
2026-06-10 15:39:30 -0300

    Exercício 31 completo

--- exercícios/Condições compostas/ex31-pedra-papel-tesoura/ex31.js
+++ exercícios/Condições compostas/ex31-pedra-papel-tesoura/ex31.js
@@ -3,19 +3,51 @@ botao.addEventListener('click', jokenpo)
 
 function jokenpo() {
     let res = document.querySelector('#res')
+    let opcao = document.getElementsByName('opcao')
+
+    res.innerHTML = '' 
+    res.style.background = ''
 
     let valor = Math.random() * 3
-    
-    let valor_formatado = Math.floor(valor)
+    let valor_formatado = Math.floor(valor) 
+    let novo_valor = valor_formatado + 1
 
-    res.innerHTML = `${valor_formatado}`
+    let jogador = null
+    let computador = null
+
+    if (opcao[0].checked) { 
+        jogador = 'Pedra'
+    } else if (opcao[1].checked) {
+        jogador = 'Papel'
+    } else {
+        jogador = 'Tesoura'
+    }
+
+    if (novo_valor == 1) {
+        computador = 'Pedra'
+    } else if (novo_valor == 2) {
+        computador = 'Papel'
+    } else {
+       computador = 'Tesoura'
+    }
+
+    if (jogador == 'Pedra' && computador == 'Tesoura' || jogador == 'Papel' && computador == 'Pedra' || jogador == 'Tesoura' && computador == 'Papel') {
+        res.innerHTML = `Sua escolha: ${jogador} <br>
+        Escolha do computador: ${computador} <br>
+        Resultado: Você venceu!`
+
+        res.style.background = 'green'
+    } else if (jogador == computador) {
+        res.innerHTML = `Sua escolha: ${jogador} <br>
+        Escolha do computador: ${computador} <br>
+        Resultado: Empatou!`
+
+        res.style.background = 'yellow'
+    } else {
+        res.innerHTML = `Sua escolha: ${jogador} <br>
+        Escolha do computador: ${computador} <br>
+        Resultado: Você perdeu!`
+
+        res.style.background = 'red'
+    }
 }
-
-
-/*  let res = document.querySelector('#res')
-
-    let valor = Math.random() * 3
-
-    let talvez_tesoura = Math.floor(valor)
-
-    res.innerHTML = `${talvez_tesoura}` */
