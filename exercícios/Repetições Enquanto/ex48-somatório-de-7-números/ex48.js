3902eca27d5c6b495b673a63d14c9226a02719f8
nicolas-gabriel020 <gabrielnicolas018@gmail.com>
2026-06-25 11:33:02 -0300

    Exercício 48 alterado

--- exercícios/Repetições Enquanto/ex48-somatório-de-7-números/ex48.js
+++ exercícios/Repetições Enquanto/ex48-somatório-de-7-números/ex48.js
@@ -1,17 +1,67 @@
 const botao = document.querySelector('#botao')
 botao.addEventListener('click', valores_adicionados)
 
-let numero = 1
+let c = 0
+let soma = 0
+let par = 0
+let impar = 0
+let media = 0
+let maior
+let menor
+
 function valores_adicionados() {
     let res = document.querySelector('#res')
     res.innerHTML = ''
 
     let valor = document.querySelector('#valor')
-
+      
     if (valor.value == '') {
-        res.innerHTML = 'Por favor digite um valor!'
+        res.innerHTML = 'Por favor preencha com todos os dados!'
     } else {
-        let falta = document.querySelector('#falta')
-        let valor_formatado = Number(valor.value)
+        let numero = Number(valor.value)
+        let decimal = Math.floor(numero)
+
+        if (numero != decimal) {
+            res.innerHTML = 'Por favor digite apenas números inteiros!'
+        } else {
+            c++
+
+            if (c == 1) {
+                maior = numero
+                menor = numero
+            } else {
+                if (numero > maior) {
+                    maior = numero
+                }
+
+                if (numero < menor) {
+                    menor = numero
+                }
+            }
+
+            if (numero % 2 == 0) {
+                par++
+            } else {
+               impar++ 
+            }
+
+            soma += numero
+
+            if (c < 7) {
+                res.innerHTML = `${c} número digitado!`
+            } else if (c == 7) {
+                media = soma / c
+                res.innerHTML = `Soma total: ${soma} <br>
+                Maior valor digita: ${maior} <br>
+                Menor valor digitado: ${menor} <br>
+                Quantidade de números pares: ${par} <br>
+                Quantidade de números impares: ${impar} <br>
+                Média dos números: ${media.toFixed(1)} <br>
+                C: ${c}`
+            } else {
+                return(res.innerHTML = 'Sequência finalizada!')
+            }
+        }
     }
 }
+
