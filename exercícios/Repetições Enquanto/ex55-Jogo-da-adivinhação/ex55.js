eea4544b65bbaece806d1b49f4c7b2f05eb8e90b
nicolas-gabriel020 <gabrielnicolas018@gmail.com>
2026-07-04 15:43:26 -0300

    Exercício 55 concluido

--- exercícios/Repetições Enquanto/ex55-Jogo-da-adivinhação/ex55.js
+++ exercícios/Repetições Enquanto/ex55-Jogo-da-adivinhação/ex55.js
@@ -12,10 +12,28 @@ let acerto = 0
 let erro = 0
 let c = 0
 let i = 4
-let diferenca = 0
-let distancia = 0
+let vitorias = 0
+let derrotas = 0
+let porcentagem = 0
+let total_partidas = 1
+let jogo_finalizado = false
 
 function sorteio() {
+    if (jogo_finalizado) {
+        jogo_finalizado = false
+        botao.innerHTML = 'Enviar palpite'
+
+        valor = Math.random() * 10
+        valor_formatado = Math.floor(valor)
+        novo_valor = valor_formatado + 1
+
+        acerto = 0
+        erro = 0
+        c = 0
+        i = 4
+        total_partidas++
+    }
+
     if (numero.value == '') {
         res.innerHTML = 'Por favor preencha todos os campos!'
 
@@ -29,85 +47,59 @@ function sorteio() {
         } else if (numero_formatado != decimal) {
             res.innerHTML = 'Preencha apenas com números inteiros!'
         } else {
+            numero.value = ''
+            numero.focus()
+
             c++
 
             if (numero_formatado == novo_valor) {
                 acerto++
             } else {
                 erro++
-
-                 diferenca = numero_formatado - novo_valor
-                 distancia = Math.abs(diferenca)
             }
 
-            if (c < 4) {
-                if (numero_formatado == novo_valor) {
-                    res.innerHTML = `Você acertou! <br><br>
-                    Você escolheu ${numero_formatado} <br>
-                    Computador escolheu ${novo_valor} <br><br>
-                    Acertos: ${acerto} <br>
-                    Erros: ${erro}`
+            if (c <= 4 && numero_formatado == novo_valor) {
+                vitorias++
+                porcentagem = (vitorias / total_partidas) * 100
+
+                res.innerHTML = `Você acertou na ${c} tentativa! <br><br>
+                Você escolheu ${numero_formatado} <br>
+                Computador escolheu ${novo_valor} <br><br>
+                Acertos: ${acerto} <br>
+                Erros: ${erro} <br><br>
+                Total de vitórias: ${vitorias} <br>
+                Total de derrotas: ${derrotas} <br><br>
+                Porcentagem de vitórias: ${porcentagem.toFixed(1)}%`
+
+                jogo_finalizado = true
+                botao.innerHTML = 'Tentar novamente'
+            } else if (c < 4) {
+                i--
+                let maior_menor = ''
+
+                if (numero_formatado > novo_valor) {
+                    maior_menor = 'maior'
                 } else {
-                    i--
-                    res.innerHTML = `Você errou! <br>
-                    Restam ${i} tentativas! <br><br>
-                    Acertos: ${acerto} <br>
-                    Erros: ${erro} <br>`
+                    maior_menor = 'menor'   
                 }
+
+                res.innerHTML = `Você errou! <br>
+                Restam ${i} tentativas! <br><br>
+                Dica: Número digitado ${maior_menor} que o sorteado!`
             } else {
+                derrotas++
+
                 res.innerHTML = `Você teve um total de ${c} tentativas <br><br>
+                Computador escolheu: ${novo_valor} <br><br>
+                Total de vitórias: ${vitorias} <br>
+                Total de derrotas: ${derrotas}<br><br>
                 Total de acertos: ${acerto} <br>
-                Total de erros: ${erro}`
+                Total de erros: ${erro} <br><br>
+                Porcentagem de vitórias: ${porcentagem.toFixed(1)}%`
+
+                jogo_finalizado = true
+                botao.innerHTML = 'Tentar novamente'
             }
         }
     }
 }
-
-/*const botao = document.querySelector('#botao')
-botao.addEventListener('click', sorteio)
-
-let acerto = 0
-let erro = 0
-
-function sorteio() {
-    let res = document.querySelector('#res')
-
-    let valor = Math.random() * 10
-    let valor_formatado = Math.floor(valor)
-    let novo_valor = valor_formatado + 1
-
-    let numero = document.querySelector('#numero')
-
-    if (numero.value == '') {
-        res.innerHTML = 'Por favor preencha todos os campos!'
-    } else {
-        let numero_formatado = Number(numero.value)
-        let decimal = Math.floor(numero_formatado)
-
-        if (numero_formatado < 1 || numero_formatado > 10) {
-            res.innerHTML = 'Número inválido!'
-        } else if (numero_formatado != decimal) {
-            res.innerHTML = 'Preencha apenas com números inteiros!'
-        } else {
-            if (numero_formatado == novo_valor) {
-                acerto++
-                res.innerHTML = `Você acertou! <br><br>
-                Você escolheu ${numero_formatado} <br>
-                Computador escolheu ${novo_valor} <br><br>
-                Acertos: ${acerto} <br>
-                Erros: ${erro}`
-            } else {
-                erro++
-                let diferenca = numero_formatado - novo_valor
-                let distancia = Math.abs(diferenca)
-
-                res.innerHTML = `Você errou! <br><br>
-                Você escolheu ${numero_formatado} <br>
-                Computador escolheu ${novo_valor} <br><br>
-                Acertos: ${acerto} <br>
-                Erros: ${erro} <br>
-                Diferença: ${distancia}`
-            }
-        }
-    }
-}*/
