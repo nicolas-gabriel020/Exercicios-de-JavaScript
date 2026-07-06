ea32a7e27e3ee0dd292c035ed8b8e5d4092cd254
nicolas-gabriel020 <gabrielnicolas018@gmail.com>
2026-07-06 18:33:52 -0300

    Exercício 56 adicionado

---
+++ exercícios/Repetições enquanto com flag/ex56-Somatório-com-valor-sentinela/ex56.js
@@ -0,0 +1,75 @@
+const botao = document.querySelector('#botao')
+botao.addEventListener('click', somatorio)
+
+let res = document.querySelector('#res')
+let valor = document.querySelector('#valor')
+let posicao = document.querySelector('#posicao')
+
+let c = 0
+let soma = 0
+let total = 0
+let media = 0
+let maior = 0
+let menor = 0
+let par = 0
+let impar = 0
+
+function somatorio() {
+    res.innerHTML = `Aguardando execução... <br><br> 
+    Observação: Ao digitar o número 1111 e executar a sequência se encerra`
+
+    if (valor.value == '') {
+        res.innerHTML = 'Por favor digite um valor'
+
+        valor.focus()
+    } else {
+        let numero = Number(valor.value)
+        let decimal = Math.floor(numero)
+        
+        if (numero != decimal || numero < 0) {
+            res.innerHTML = 'Por favor digite apenas números válidos!'
+
+            valor.focus()
+        } else {
+            valor.value = ''
+            valor.focus()
+
+            if (numero !== 1111) {
+                c++
+                soma += numero
+                total++
+
+                if (c == 1) {
+                    maior = numero
+                    menor = numero
+                } else {
+                    if (numero > maior) {
+                        maior = numero
+                    }
+
+                    if (numero < menor) {
+                        menor = numero
+                    }
+                }
+
+                if (numero % 2 == 0) {
+                    par++
+                } else {
+                    impar++
+                }
+
+                posicao.innerHTML = `Digite o ${c + 1}º número`
+            } else {
+                media = soma / total
+
+                res.innerHTML = `Total de números digitados: ${c} <br>
+                Soma de todos os números: ${soma} <br>
+                Média dos números: ${media} <br><br>
+                Maior número digitado: ${maior} <br>
+                Menor número digitado: ${menor} <br><br>
+                Total de números pares: ${par} <br>
+                Total de números ímpares: ${impar}`
+            }
+        }
+    }
+}
