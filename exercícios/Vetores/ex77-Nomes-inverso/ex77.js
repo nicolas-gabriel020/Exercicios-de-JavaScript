cb654b233db2c99f3c742022aaca6e60d5e7d93f
nicolas-gabriel020 <gabrielnicolas018@gmail.com>
2026-08-06 15:47:48 -0300

    Update ex77.js

--- exercícios/Vetores/ex77-Nomes-inverso/ex77.js
+++ exercícios/Vetores/ex77-Nomes-inverso/ex77.js
@@ -4,10 +4,12 @@ adicionar.addEventListener('click', adicionar_nome)
 let res = document.querySelector('#res')
 let nome = document.querySelector('#nome')
 
+let Todos_nomes = []
+
 function adicionar_nome() {
     if (nome.value == '') {
         alert('erro ao adicionar nome!')
     } else {
-        
+        Todos_nomes.push = nome.value
     }
 }
