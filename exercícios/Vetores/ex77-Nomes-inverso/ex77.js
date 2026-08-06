5b4bbfff03b1267d1aeb8ccdef3dc30b8286b348
nicolas-gabriel020 <gabrielnicolas018@gmail.com>
2026-08-06 11:50:43 -0300

    Exercício 77 ainda não concluído

---
+++ exercícios/Vetores/ex77-Nomes-inverso/ex77.js
@@ -0,0 +1,13 @@
+const adicionar = document.querySelector('#executar')
+adicionar.addEventListener('click', adicionar_nome)
+
+let res = document.querySelector('#res')
+let nome = document.querySelector('#nome')
+
+function adicionar_nome() {
+    if (nome.value == '') {
+        alert('erro ao adicionar nome!')
+    } else {
+        
+    }
+}
